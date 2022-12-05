46cd7b4408f8cd476cd44105eab5cef06e429a35
Stone Comstock <comstockstone@yahoo.com>
2022-12-05 15:26:08 -0800

    Added custom error handler middleware.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -7,6 +7,10 @@ const getGoals = (req, res) => {
 // Set a Goal
 // POST /api/goals
 const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field!');
+  }
   res.status(200).json({ message: 'Set goal!' });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5500;
 
 const app = express();
 
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
+
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
