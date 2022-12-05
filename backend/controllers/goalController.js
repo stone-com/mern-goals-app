97be7b085c033611f82e1b2d4e5333faacb32c57
Stone Comstock <comstockstone@yahoo.com>
2022-12-04 18:57:24 -0800

    Set up basic CRUD routes and controller functions for Goal routes.

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,30 @@
+// Get goals
+// GET /api/goals
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'Get goals!' });
+};
+
+// Set a Goal
+// POST /api/goals
+const setGoal = (req, res) => {
+  res.status(200).json({ message: 'Set goal!' });
+};
+
+// Update a goal
+// GET /api/goals/:id
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// Delete a goal
+// GET /api/goals/:id
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hey World!');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5500;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
