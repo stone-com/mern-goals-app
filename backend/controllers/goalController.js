a2bf3b27ad3c851f0c02fd69830987aea5b0cab2
Stone Comstock <comstockstone@yahoo.com>
2022-12-05 15:30:31 -0800

    Added async handler package for controllers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,32 @@
+const asyncHandler = requier('express-async-handler');
+
 // Get goals
 // GET /api/goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals!' });
-};
+});
 
 // Set a Goal
 // POST /api/goals
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field!');
   }
   res.status(200).json({ message: 'Set goal!' });
-};
+});
 
 // Update a goal
 // GET /api/goals/:id
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // Delete a goal
 // GET /api/goals/:id
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
