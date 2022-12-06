d46670b1620832672aedca9a205161deb3099e9b
Stone Comstock <comstockstone@yahoo.com>
2022-12-06 15:34:14 -0800

    Get goals function working.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,39 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import GoalForm from '../components/GoalForm';
+// import GoalItem from '../components/GoalItem';
+import Spinner from '../components/Spinner';
+import { getGoals, reset } from '../features/goals/goalSlice';
 
-const Dashboard = () => {
+function Dashboard() {
   const navigate = useNavigate();
-  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  // Redirect to login page if user is not logged in
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
+
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate('/login');
     }
-  }, [user, navigate]);
+
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -20,8 +41,22 @@ const Dashboard = () => {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+
       <GoalForm />
+
+      {/* <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section> */}
     </>
   );
-};
+}
+
 export default Dashboard;
