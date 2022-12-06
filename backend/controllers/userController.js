44d8658be152955d1d5b1b93f4c49058499a1a79
Stone Comstock <comstockstone@yahoo.com>
2022-12-05 17:09:51 -0800

    User login controller. no JWT yet

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,6 +41,17 @@ const registerUser = asyncHandler(async (req, res) => {
 // Authenticate a user
 // POST /api/users/login
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+  // Compare password from body to user password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({ _id: user.id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error('Invalid Credentials');
+  }
   res.json({ message: 'Login User' });
 });
 
