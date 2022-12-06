4f8f575df6b5e8928598c397e03db33f1a862a1b
Stone Comstock <comstockstone@yahoo.com>
2022-12-05 21:04:50 -0800

    Refactored part of userController and goalController

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -37,15 +37,13 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found');
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for the user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error('User not found');
   }
   // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error('User is not authorized');
   }
